ba022db7e50b0bd1929d909d33a423e9a8b24052
Thiago Oss Emer Eizerik <49419328+ThiagoEize@users.noreply.github.com>
2024-11-25 16:28:52 -0300

    filter authors by name

--- src/components/shared/author-table/index.tsx
+++ src/components/shared/author-table/index.tsx
@@ -3,6 +3,9 @@
 import { DataTable } from '@/components/shared/data-table';
 import { IAuthor } from '@/lib/types/authors';
 import { AuthorColumns } from './columns';
+import { useDebounce } from '@/hooks/useDebounce';
+import { Input } from '@/components/ui/input';
+import { useMemo, useState } from 'react';
 interface AuthorTableProps {
   authors: IAuthor[];
 }
@@ -12,9 +15,27 @@ export default function AuthorTable({
 }: AuthorTableProps) {
   const columns = AuthorColumns();
 
+  const [searchQuery, setSearchQuery] = useState('');
+  const debouncedQuery = useDebounce(searchQuery, 1000);
+
+  const filteredAuthors = useMemo(() => {
+    if (!debouncedQuery) return authors;
+    return authors?.filter((book) =>
+      book?.title?.toLowerCase().includes(debouncedQuery.toLowerCase())
+    );
+  }, [debouncedQuery, authors]);
+
   return (
     <div>
-      <DataTable columns={columns} data={authors} />
+      <div className="flex mb-4 gap-2">
+        <Input
+          placeholder="Search authors by name..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="2-20"
+        />
+      </div>
+      <DataTable columns={columns} data={filteredAuthors} />
     </div>
   );
 }
